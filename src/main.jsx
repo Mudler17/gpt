a266ac533d6d8ff252ccdbc664677542a75d97d9
Mudler17 <62061316+Mudler17@users.noreply.github.com>
2026-05-06 15:28:15 +0200

    Switch entrypoint to App21 project dossier 1.6

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App20.jsx";
+import App from "./App21.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
