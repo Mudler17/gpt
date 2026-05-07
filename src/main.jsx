180d77d04c71ec59e533e8071df6bf36c2098686
Mudler17 <62061316+Mudler17@users.noreply.github.com>
2026-05-07 16:55:53 +0200

    Activate App 3.1 redundancy and navigation check

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App31.jsx";
+import App from "./App32.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
